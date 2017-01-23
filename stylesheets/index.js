9bee25afde22746a49ef3191ee16556ac6f014ad
Talal Talhouk <talal@sbcglobal.net>
2017-01-23 08:57:59 -0800

    restyle portfolio.  Set up bootstrap and vendor files

--- stylesheets/index.js
+++
@@ -1,4 +0,0 @@
-var dow = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var today = new Date();
-var day = document.getElementById("day");
-day.textContent = dow[today.getDay()];
